2e4f8462c6de273e260129edb05ba8894be96230
Jeswer <kost@itu.dk>
2024-03-15 12:59:02 +0100

    javadoc

--- KlogAI.java
+++ KlogAI.java
@@ -2,60 +2,82 @@ import java.util.ArrayList;
 
 public class KlogAI implements IOthelloAI {
 
+    /**
+     * Takes the calculaled max score and calls the associated position of the move
+     * @param s The current game state
+     * @return The position of the movescore
+     */
     public Position decideMove(GameState s) {
-        return maxValue(s, Integer.MIN_VALUE, Integer.MAX_VALUE).getPosition();
+        MoveScore fMoveScore = maxValue(s, Integer.MIN_VALUE, Integer.MAX_VALUE, 6);
+        return fMoveScore.getPosition();
     }
 
-    public MoveScore maxValue(GameState s, int alpha, int beta) {
-        ArrayList<Position> moves = s.legalMoves();
-        Position bestMove = new Position(0, 0);
-        if (s.isFinished()) {
-            return null;
+    /**
+     * Calculate the best possible move for the player based on:
+     * @param s The current game state
+     * @param alpha The lowest possible min value (used for prunning)
+     * @param beta The highest possible max value (used for prunning)
+     * @param depth The depth of recurrision used for search
+     * @return the most valuable move
+     */
+    private MoveScore maxValue(GameState s, int alpha, int beta, int depth) {
+        ArrayList<Position> legalMoves = s.legalMoves();
+        Position pos = null;
+        if (depth == 0 || legalMoves.isEmpty()) {
+            int util = Utility.evaluateGameState(s);
+            return new MoveScore(util, null);
         } else {
-            int v = Integer.MAX_VALUE;
-            GameState newState = new GameState(s.getBoard(), s.getPlayerInTurn());
-            for (Position move : moves) {
-                s.insertToken(move, null);
-                MoveScore scoreMove = minValue(newState, alpha, beta);
-                int v2 = scoreMove.getScore();
+            int v = Integer.MIN_VALUE;
+            for (Position p : legalMoves) {
+                GameState nGameState = new GameState(s.getBoard(), s.getPlayerInTurn()); 
+                nGameState.insertToken(p, null);
+                int v2 = minValue(nGameState, alpha, beta, depth - 1).getScore();
+                
                 if (v2 > v) {
                     v = v2;
-                    bestMove = move;
-                }
-                alpha = Math.max(alpha, v);
-                if (alpha >= beta) {
-                    break;
-                }
-            }
-            return new MoveScore(v, bestMove);
+                    pos = p;
+                    alpha = Math.max(alpha, v);
+                } 
 
-        }
-    }
-
-    public MoveScore minValue(GameState s, int alpha, int beta) {
-        ArrayList<Position> moves = s.legalMoves();
-        Position bestMove = new Position(0, 0);
-        if (s.isFinished()) {
-            return null;
-        } else {
-            int v = Integer.MAX_VALUE;
-            GameState newState = new GameState(s.getBoard(), s.getPlayerInTurn());
-            for (Position move : moves) {
-                s.insertToken(move, null);
-                MoveScore scoreMove = maxValue(newState, alpha, beta);
-                int v2 = scoreMove.getScore();
-                if (v2 > v) {
-                    v = v2;
-                    bestMove = move;
-                }
-
-                alpha = Math.max(alpha, v);
                 if (v >= beta) {
-                    break;
+                    return new MoveScore(v, pos);
+                } 
+            }
+            return new MoveScore(v, pos);
+        }
+
+    }
+    /**
+     * Calculate the best possible move for the opponent based on: 
+     * @param s The current game state
+     * @param alpha The lowest possible min value (used for prunning)
+     * @param beta The highest possible max value (used for prunning)
+     * @param depth The depth of recursive steps used for search
+     * @return The least valuable move
+     */
+    private MoveScore minValue(GameState s, int alpha, int beta, int depth) {
+        ArrayList<Position> legalMoves = s.legalMoves();
+        Position pos = null;
+        if(depth == 0 || legalMoves.isEmpty()) {
+            int util = Utility.evaluateGameState(s);
+            return new MoveScore(util, null);
+        } else {
+            int v = Integer.MAX_VALUE;
+            for (Position p : legalMoves) {
+                GameState nGameState = new GameState(s.getBoard(), s.getPlayerInTurn()); 
+                nGameState.insertToken(p, null);
+                int v2 = maxValue(nGameState, alpha, beta, depth - 1).getScore();
+                if(v2 < v) {
+                    v = v2;
+                    pos = p;
+                    beta = Math.min(beta,v);
+                }
+                if(v <= alpha) {
+                    return new MoveScore(v, pos);
                 }
             }
-            return new MoveScore(v, bestMove);
-
+            return new MoveScore(v, pos);
         }
     }
+
 }
